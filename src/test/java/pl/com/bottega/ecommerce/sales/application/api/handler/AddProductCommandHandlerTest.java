409325b03d8d6160164a3462b3b616c309588615
210952 Skowronski Pawel <210952@edu.p.lodz.pl>
2019-04-10 19:24:44 +0200

    Test checking suggestionSErvice not called when product is available

--- src/test/java/pl/com/bottega/ecommerce/sales/application/api/handler/AddProductCommandHandlerTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/application/api/handler/AddProductCommandHandlerTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.mockito.Mockito;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
 import pl.com.bottega.ecommerce.sales.application.api.command.AddProductCommand;
+import pl.com.bottega.ecommerce.sales.domain.client.Client;
 import pl.com.bottega.ecommerce.sales.domain.client.ClientRepository;
 import pl.com.bottega.ecommerce.sales.domain.equivalent.SuggestionService;
 import pl.com.bottega.ecommerce.sales.domain.productscatalog.Product;
@@ -34,5 +35,26 @@ public class AddProductCommandHandlerTest {
 
         AddProductCommandHandler addProductCommandHandler = new AddProductCommandHandler(reservationRepository, productRepository, suggestionService, clientRepository, systemContext);
         assertNull(addProductCommandHandler.handle(new AddProductCommand(Id.generate(), Id.generate(), 1)));
+    }
+
+    @Test
+    public void shouldNotCallSuggestionServiceWhenProductIsAvailable() {
+        ReservationRepository reservationRepository = Mockito.mock(ReservationRepository.class);
+        Reservation reservation = Mockito.mock(Reservation.class);
+        ProductRepository productRepository = Mockito.mock(ProductRepository.class);
+        SuggestionService suggestionService = Mockito.mock(SuggestionService.class);
+        ClientRepository clientRepository = Mockito.mock(ClientRepository.class);
+        SystemContext systemContext = Mockito.mock(SystemContext.class);
+        Product product = Mockito.mock(Product.class);
+        Mockito.when(reservationRepository.load(any(Id.class))).thenReturn(reservation);
+        Mockito.when(productRepository.load(any(Id.class))).thenReturn(product);
+        Mockito.when(product.isAvailable()).thenReturn(true);
+        Mockito.doNothing().when(reservation).add(any(Product.class), any(int.class));
+        Mockito.doNothing().when(reservationRepository).save(any(Reservation.class));
+
+        AddProductCommandHandler addProductCommandHandler = new AddProductCommandHandler(reservationRepository, productRepository, suggestionService, clientRepository, systemContext);
+        addProductCommandHandler.handle(new AddProductCommand(Id.generate(), Id.generate(), 1));
+
+        Mockito.verify(suggestionService, Mockito.never()).suggestEquivalent(any(Product.class), any(Client.class));
     }
 }
